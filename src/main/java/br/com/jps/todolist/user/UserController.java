0b82ce91225ad30a99b0c613a0480c3ab7a76888
Jessica Sousa <jessica.persou@gmail.com>
2023-10-14 22:11:45 -0300

    rockeatseat aula 2

--- src/main/java/br/com/jps/todolist/user/UserController.java
+++ src/main/java/br/com/jps/todolist/user/UserController.java
@@ -1,5 +1,8 @@
 package br.com.jps.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private IUserRepository userRepository;
+
     @PostMapping
-    public void create(@RequestBody UserModel userModel){
-        System.out.println(userModel.name);
+    public ResponseEntity create(@RequestBody UserModel userModel){
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+        UserModel userSave = this.userRepository.save(userModel);
+        if(user != null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe.");
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).body(userSave);
     }
 }
--- src/main/java/br/com/jps/todolist/user/UserModel.java
+++ src/main/java/br/com/jps/todolist/user/UserModel.java
@@ -1,9 +1,28 @@
 package br.com.jps.todolist.user;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity(name = "users")
 public class UserModel {
 
-    public String name;
-    public String username;
-    public String password;
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+    private String name;
+    private String username;
+    private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createAt;
 
 }
